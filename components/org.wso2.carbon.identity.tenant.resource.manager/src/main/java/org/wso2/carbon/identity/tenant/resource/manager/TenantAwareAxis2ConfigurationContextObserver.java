6b73ce4d621a7ced40edca7e092e5ec87cd631df
Buddhima <buddhimau@wso2.com>
2019-09-20 11:30:35 +0530

    initial commit for c4

---
+++ components/org.wso2.carbon.identity.tenant.resource.manager/src/main/java/org/wso2/carbon/identity/tenant/resource/manager/TenantAwareAxis2ConfigurationContextObserver.java
@@ -0,0 +1,4 @@
+package org.wso2.carbon.identity.tenant.resource.manager;
+
+public class TenantAwareAxis2ConfigurationContextObserver {
+}
---
+++ components/org.wso2.carbon.identity.tenant.resource.manager/src/main/java/org/wso2/carbon/identity/tenant/resource/manager/internal/EmailEventAdapterFactoryDataHolder.java
@@ -0,0 +1,4 @@
+package org.wso2.carbon.identity.tenant.resource.manager.internal;
+
+public class EmailEventAdapterFactoryDataHolder {
+}
